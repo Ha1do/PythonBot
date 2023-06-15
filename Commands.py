a2b2ae02fef4d94c0ad0b7522996b428820306a9
Ha1do <gogono76@gmail.com>
2023-06-16 00:43:16 +0300

    made a command file functional

--- Commands.py
+++ Commands.py
@@ -1,47 +1,47 @@
-# from telegram import *
-# from telegram.ext import *
-#
-# #Commands
-# async def start_command (update: Update, context: ContextTypes.DEFAULT_TYPE):
-#     await update.message.reply_text("Hello")
-#
-# async def help_command (update: Update, context: ContextTypes.DEFAULT_TYPE):
-#     await update.message.reply_text("U called about help, so here i am"
-#                               "/start -> starts a bot"
-#                               "/help -> call this message"
-#                               "/pin -> pins a message"
-#                               "/sendmedia -> sends a social media post"
-#                               "/warn"
-#                               "/kick"
-#                               "/ban"
-#                               "/setrules"
-#                               "/report"
-#                               "/blacklistadd"
-#                               "/blacklistremove")
-#
-# async def pin_command (update: Update, context: ContextTypes.DEFAULT_TYPE):
-#     await update.message.reply_text("pinned text XD")
-#
-# async def sendmedia_command (update: Update, context: ContextTypes.DEFAULT_TYPE):
-#     await update.message.reply_text("i must send some social media but now i can't")
-#
-# async def warn_command (update: Update, context: ContextTypes.DEFAULT_TYPE):
-#     await update.message.reply_text("let's think i waned u")
-#
-# async def kick_command (update: Update, context: ContextTypes.DEFAULT_TYPE):
-#     await update.message.reply_text("i think now u can't be here, but if u already there - u can stay")
-#
-# async def ban_command (update: Update, context: ContextTypes.DEFAULT_TYPE):
-#     await update.message.reply_text("BAN-HAMMER")
-#
-# async def setrules_command (update: Update, context: ContextTypes.DEFAULT_TYPE):
-#     await update.message.reply_text("i gess, now u need to set rules but don't do this pls")
-#
-# async def report_command (update: Update, context: ContextTypes.DEFAULT_TYPE):
-#     await update.message.reply_text("report LOL")
-#
-# async def blacklistadd_command (update: Update, context: ContextTypes.DEFAULT_TYPE):
-#     await update.message.reply_text("suka will never be in black list")
-#
-# async def blacklistremove_command (update: Update, context: ContextTypes.DEFAULT_TYPE):
-#     await update.message.reply_text("Хайдо п*дор will never be removed from black list")
+from telegram import *
+from telegram.ext import *
+
+#Commands
+async def start_command (update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Hello")
+
+async def help_command (update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("U called about help, so here i am"
+                              "/start -> starts a bot"
+                              "/help -> call this message"
+                              "/pin -> pins a message"
+                              "/sendmedia -> sends a social media post"
+                              "/warn"
+                              "/kick"
+                              "/ban"
+                              "/setrules"
+                              "/report"
+                              "/blacklistadd"
+                              "/blacklistremove")
+
+async def pin_command (update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("pinned text XD")
+
+async def sendmedia_command (update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("i must send some social media but now i can't")
+
+async def warn_command (update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("let's think i waned u")
+
+async def kick_command (update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("i think now u can't be here, but if u already there - u can stay")
+
+async def ban_command (update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("BAN-HAMMER")
+
+async def setrules_command (update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("i gess, now u need to set rules but don't do this pls")
+
+async def report_command (update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("report LOL")
+
+async def blacklistadd_command (update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("suka will never be in black list")
+
+async def blacklistremove_command (update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Хайдо п*дор will never be removed from black list")
--- main.py
+++ main.py
@@ -1,53 +1,9 @@
 from telegram import *
 from telegram.ext import *
+import Commands
 
 API_TOKEN = '1256415211:AAGQfWloyT0H91OrMlmfsM17IUFS7YVmDWg'
 BOT_USERNAME = "@Ha1do_bot"
-
-#Commands
-async def start_command (update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Hello")
-
-async def help_command (update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("U called about help, so here i am"
-                              "/start -> starts a bot"
-                              "/help -> call this message"
-                              "/pin -> pins a message"
-                              "/sendmedia -> sends a social media post"
-                              "/warn"
-                              "/kick"
-                              "/ban"
-                              "/setrules"
-                              "/report"
-                              "/blacklistadd"
-                              "/blacklistremove")
-
-async def pin_command (update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("pinned text XD")
-
-async def sendmedia_command (update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("i must send some social media but now i can't")
-
-async def warn_command (update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("let's think i waned u")
-
-async def kick_command (update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("i think now u can't be here, but if u already there - u can stay")
-
-async def ban_command (update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("BAN-HAMMER")
-
-async def setrules_command (update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("i gess, now u need to set rules but don't do this pls")
-
-async def report_command (update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("report LOL")
-
-async def blacklistadd_command (update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("suka will never be in black list")
-
-async def blacklistremove_command (update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Хайдо п*дор will never be removed from black list")
 
 #Response
 def handle_response (text: str) -> str:
@@ -83,9 +39,17 @@ if __name__ == "__main__":
     app = Application.builder().token(API_TOKEN).build()
 
     #Commands
-    app.add_handler(CommandHandler("start", start_command))
-    app.add_handler(CommandHandler("help", help_command))
-    app.add_handler(CommandHandler("pin", pin_command))
+    app.add_handler(CommandHandler("start", Commands.start_command))
+    app.add_handler(CommandHandler("help", Commands.help_command))
+    app.add_handler(CommandHandler("pin", Commands.pin_command))
+    app.add_handler(CommandHandler("sendmedia", Commands.sendmedia_command))
+    app.add_handler(CommandHandler("warn", Commands.warn_command))
+    app.add_handler(CommandHandler("kick", Commands.kick_command))
+    app.add_handler(CommandHandler("ban", Commands.ban_command))
+    app.add_handler(CommandHandler("setrules", Commands.setrules_command))
+    app.add_handler(CommandHandler("report", Commands.report_command))
+    app.add_handler(CommandHandler("blacklistadd", Commands.blacklistadd_command))
+    app.add_handler(CommandHandler("blacklistremove", Commands.blacklistremove_command))
 
     #Messages
     app.add_handler(MessageHandler(filters.TEXT, handle_message))
